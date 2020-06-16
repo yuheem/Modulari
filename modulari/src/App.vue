11961d89688bd65406e74008a1002573408e9fed
jx <choojiaxin@gmail.com>
2020-06-17 04:41:23 +0800

    added filter options

--- modulari/src/App.vue
+++ modulari/src/App.vue
@@ -38,6 +38,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-  align-items: stretch;
 }
 </style>
--- modulari/src/components/Sidebar.vue
+++ modulari/src/components/Sidebar.vue
@@ -1,12 +1,40 @@
 <template>
   <div id="sidebar">
     <h4>Filter by:</h4>
+    <form>
+      <ul>
+        <li>
+          <label for="level">Level:</label>
+          <input type="text" name="level" placeholder="No filter" />
+        </li>
+        <li>
+          <label for="faculty">Faculty:</label>
+          <input type="text" name="faculty" placeholder="No filter" />
+        </li>
+        <li>
+          <label for="numOfMCs">Number of MCs:</label>
+          <input type="text" name="numOfMCs" placeholder="No filter" />
+        </li>
+        <li>
+          <label for="exams">Exams:</label>
+          <input type="text" name="exams" placeholder="No filter" />
+        </li>
+      </ul>
+    </form>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Sidebar"
+  name: "Sidebar",
+  data() {
+    return {
+      level: "",
+      faculty: "",
+      numOfMCs: "",
+      exams: ""
+    };
+  }
 };
 </script>
 
@@ -14,5 +42,8 @@ export default {
 #sidebar {
   min-width: 250px;
   max-width: 250px;
+}
+ul {
+  list-style: none;
 }
 </style>
