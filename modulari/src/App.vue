f47249c8628c0eb228aa5b6b64e5e4710c14ba5c
jx <choojiaxin@gmail.com>
2020-06-19 02:48:07 +0800

    Retrieve data from NUSMods API

--- modulari/src/App.vue
+++ modulari/src/App.vue
@@ -7,6 +7,8 @@
 </template>
 
 <script>
+import axios from "axios";
+
 import AddModules from "./components/AddModules";
 import Sidebar from "./components/Sidebar";
 import Header from "./components/layout/Header";
@@ -20,14 +22,17 @@ export default {
   },
   data() {
     return {
-      moduleData: []
+      modulesShownInGraph: []
     };
   },
   methods: {
     generateTree(moduleCode) {
-      // testing functionality
-      this.moduleData = [...this.moduleData, moduleCode];
-      console.log(this.moduleData);
+      // Retrieve module information from NUSMods
+      axios
+        .get(`https://api.nusmods.com/v2/2019-2020/modules/${moduleCode}.json`)
+        .then(res => this.modulesShownInGraph.push(res.data))
+        .catch(err => console.log(err));
+      console.log(this.modulesShownInGraph);
     }
   }
 };
