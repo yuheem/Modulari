fce1dd90ed09e4a6777c11745e0da1ce0d06d160
jx <choojiaxin@gmail.com>
2020-06-24 03:26:14 +0800

    Add filter options

---
+++ modulari/src/components/FilterOptions.vue
@@ -0,0 +1,135 @@
+<template>
+  <div>
+    <form @submit="filterModules">
+      <ul>
+        <!-- Filter modules by level -->
+        <li>
+          <label for="level">Level:</label>
+          <select v-model="level" name="level">
+            <option selected>All levels</option>
+            <option v-for="lvl in levelOptions" v-bind:key="lvl.id">{{lvl.level}}</option>
+          </select>
+        </li>
+
+        <!-- Filter modules by faculty -->
+        <li>
+          <label for="faculty">Faculty:</label>
+          <select v-model="faculty" name="faculty">
+            <option selected>All faculties</option>
+            <option v-for="fac in facultyOptions" :key="fac.id">{{fac.faculty}}</option>
+          </select>
+        </li>
+
+        <!-- Filter modules by number of MCs -->
+        <li>
+          <label for="numOfMCs">Number of MCs:</label>
+          <select v-model="numOfMCs" name="numOfMCs">
+            <option selected>All MCs</option>
+            <option v-for="mc in numOfMCsOptions" v-bind:key="mc.id">{{mc.MCs}}</option>
+          </select>
+        </li>
+
+        <!-- Filter modules by whether modules is examinable -->
+        <li>
+          <label for="exams">Exams:</label>
+          <select v-model="exams" name="exams">
+            <option selected>No filter</option>
+            <option>No Exams</option>
+          </select>
+        </li>
+      </ul>
+
+      <input type="submit" value="Filter" />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FilterOptions",
+  data() {
+    return {
+      level: "",
+      faculty: "",
+      numOfMCs: "",
+      exams: "",
+      levelOptions: [
+        { id: 1, level: "1000" },
+        { id: 2, level: "2000" },
+        { id: 3, level: "3000" },
+        { id: 4, level: "4000" },
+        { id: 5, level: "5000" },
+        { id: 6, level: "6000" },
+        { id: 7, level: "7000" },
+        { id: 8, level: "8000" }
+      ],
+      facultyOptions: [
+        { id: 1, faculty: "Arts and Social Science" },
+        { id: 2, faculty: "Computing" },
+        { id: 3, faculty: "Cont and Lifelong Education" },
+        { id: 4, faculty: "Dentistry" },
+        { id: 5, faculty: "Design and Environment" },
+        { id: 6, faculty: "Duke-NUS Medical School" },
+        { id: 7, faculty: "Engineering" },
+        { id: 8, faculty: "Institute of Systems Science" },
+        { id: 9, faculty: "Law" },
+        { id: 10, faculty: "LKY School and Public Policy" },
+        { id: 11, faculty: "Logistics Inst-Asia Pac" },
+        { id: 12, faculty: "Multi Disciplinary Programme" },
+        { id: 13, faculty: "NUS" },
+        { id: 14, faculty: "NUS Business School" },
+        { id: 15, faculty: "Residential College" },
+        { id: 16, faculty: "Risk Management Institute" },
+        { id: 17, faculty: "Science" },
+        { id: 18, faculty: "SSH School of Public Health" },
+        { id: 19, faculty: "Temasek Defence Sys. Institute" },
+        { id: 20, faculty: "University Scholars Programme" },
+        { id: 21, faculty: "Yale-NUS College" },
+        { id: 22, faculty: "Yong Loo Lin Sch of Medicine" },
+        { id: 23, faculty: "YST Conservatory of Music" }
+      ],
+      numOfMCsOptions: [
+        { id: 1, MCs: "0-3 MCs" },
+        { id: 2, MCs: "4 MCs" },
+        { id: 3, MCs: "5-8 MCs" },
+        { id: 4, MCs: "More than 8 MCs" }
+      ]
+    };
+  },
+  methods: {
+    filterModules(event) {
+      event.preventDefault();
+      let filterDetails = {
+        filteredLevel: this.level,
+        filteredFaculty: this.faculty,
+        filteredNumOfMCs: this.numOfMCs,
+        filteredExams: this.exams
+      };
+
+      // Send filter details up to parent
+      this.$emit("filter-modules", filterDetails);
+    }
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  padding: 0px;
+}
+
+li {
+  padding: 10px;
+}
+
+label,
+select {
+  display: block;
+  width: 90%;
+}
+
+input[type="submit"] {
+  margin-right: 40px;
+}
+</style>>
--- modulari/src/components/Sidebar.vue
+++ modulari/src/components/Sidebar.vue
@@ -2,59 +2,23 @@
   <div id="sidebar">
     <button id="closeSidebar" @click="closeSidebar()">x</button>
     <h4>Filter by:</h4>
-    <form @submit="filterModules">
-      <ul>
-        <li>
-          <label for="level">Level:</label>
-          <input type="text" v-model="level" name="level" placeholder="No filter" />
-        </li>
-        <li>
-          <label for="faculty">Faculty:</label>
-          <input type="text" v-model="faculty" name="faculty" placeholder="No filter" />
-        </li>
-        <li>
-          <label for="numOfMCs">Number of MCs:</label>
-          <input type="text" v-model="numOfMCs" name="numOfMCs" placeholder="No filter" />
-        </li>
-        <li>
-          <label for="exams">Exams:</label>
-          <input type="text" v-model="exams" name="exams" placeholder="No filter" />
-        </li>
-      </ul>
-
-      <input type="submit" value="Filter" />
-    </form>
+    <FilterOptions v-on:filter-modules="$emit('filter-modules', filterDetails)" />
   </div>
 </template>
 
 <script>
+import FilterOptions from "./FilterOptions";
+
 export default {
   name: "Sidebar",
-  data() {
-    return {
-      level: "",
-      faculty: "",
-      numOfMCs: "",
-      exams: ""
-    };
+  components: {
+    FilterOptions
   },
   methods: {
     closeSidebar() {
       document.getElementById("main").style.marginLeft = "0%";
       document.getElementById("sidebar").style.display = "none";
       document.getElementById("openSidebar").style.display = "inline-block";
-    },
-    filterModules(event) {
-      event.preventDefault();
-      let filterDetails = {
-        filteredLevel: this.level,
-        filteredFaculty: this.faculty,
-        filteredNumOfMCs: this.numOfMCs,
-        filteredExams: this.exams
-      };
-
-      // Send filter details up to parent
-      this.$emit("filter-modules", filterDetails);
     }
   }
 };
@@ -64,16 +28,9 @@ export default {
 #sidebar {
   display: none;
 }
-ul {
-  list-style: none;
-  padding: 0px;
-}
+
 button {
   cursor: pointer;
-  float: right;
-}
-
-input[type="submit"] {
   float: right;
 }
 </style>
