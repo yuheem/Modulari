f616882c1e9d39a70c463c93eab2877ec893c989
ChooJiaXin <e0426255@u.nus.edu>
2020-07-12 08:44:44 +0800

    Add dragging and zooming features and adjust distance between nodes

--- modulari/src/components/TestGraph.vue
+++ modulari/src/components/TestGraph.vue
@@ -1,12 +1,11 @@
 <template>
   <div>
-    <svg width="100%" height="100vh" id="test-graph" v-if="nodes.length !== 0">
-      <!-- <rect></rect> -->
+    <svg width="100%" height="100vh" id="test-graph">
       <defs>
         <marker
           id="arrow"
           viewBox="0 -5 10 10"
-          refX="30"
+          refX="60"
           refY="0"
           markerWidth="6"
           markerHeight="6"
@@ -15,35 +14,8 @@
           <path d="M0,-5 L10,0 L0,5" />
         </marker>
       </defs>
-
-      <g id="links">
-        <path
-          v-for="link in links"
-          :key="link.index"
-          marker-end="url(#arrow)"
-          stroke="grey"
-          stroke-width="1.5px"
-          v-bind:d="
-            `M ${Math.max(20, link.source.x)},${Math.max(20, link.source.y)} L ${Math.max(20, link.target.x)},${Math.max(20, link.target.y)}`
-          "
-        />
-      </g>
-
-      <g v-for="node in nodes" :key="node.name">
-        <circle
-          fill="lightblue"
-          v-bind:transform="`translate(${Math.max(20, node.x)},${Math.max(20, node.y)})`"
-          r="20"
-        />
-        <text
-          font="10px san-serif"
-          color="black"
-          x="0"
-          y="0.31em"
-          text-anchor="middle"
-          v-bind:transform="`translate(${Math.max(20, node.x)},${Math.max(20, node.y)})`"
-        >{{node.name}}</text>
-      </g>
+      <rect width="100%" height="100vh" fill="blanchedalmond" />
+      <g />
     </svg>
   </div>
 </template>
@@ -57,24 +29,27 @@ export default {
   props: ["nodes", "links"],
   data() {
     return {
+      width: null,
+      height: null,
+      graph: null,
       simulation: null,
       forceProperties: {
         charge: {
-          strength: -500,
-          distanceMin: 100,
-          distanceMax: 200
+          strength: -2000,
+          distanceMin: 1,
+          distanceMax: 2000
         },
         collide: {
           strength: 0.7,
           iterations: 1,
-          radius: 35
+          radius: 100
         },
         forceX: {
           strength: 0.1,
           x: 0.5
         },
         forceY: {
-          strength: 0.35,
+          strength: 0.2,
           y: 0.5
         },
         link: {
@@ -98,26 +73,98 @@ export default {
       .on("tick", this.tick);
 
     console.log("simulation creation complete!");
+
+    this.setForceProperties();
+    console.log("force properties set");
+  },
+  mounted() {
+    this.width = document
+      .getElementById("test-graph")
+      .getBoundingClientRect().width;
+    this.height = document
+      .getElementById("test-graph")
+      .getBoundingClientRect().height;
+
+    this.graph = d3.select("g");
+
+    d3.select("svg").call(
+      d3.zoom().on("zoom", () => {
+        this.graph.attr("transform", d3.event.transform);
+      })
+    );
   },
   methods: {
-    // tick() {
-    //   this.updateSimulation();
-    //   this.setForceProperties();
-    // },
+    tick() {
+      console.log("tick");
+      const nodeCoord = node => {
+        return "translate(" + node.x + "," + node.y + ")";
+      };
+      const linkCoord = link => {
+        return (
+          "M " +
+          link.target.x +
+          "," +
+          link.target.y +
+          " L" +
+          link.source.x +
+          "," +
+          link.source.y
+        );
+      };
+
+      this.graph.selectAll("path").attr("d", linkCoord);
+      this.graph.selectAll("circle").attr("transform", nodeCoord);
+      this.graph.selectAll("text").attr("transform", nodeCoord);
+    },
     updateSimulation() {
       this.simulation.nodes(this.nodes);
       this.simulation.force("link").links(this.links);
-      console.log("yay");
+
+      this.graph.selectAll("path").remove();
+      this.graph
+        .selectAll("path")
+        .data(this.simulation.force("link").links())
+        .join("path")
+        .attr("marker-end", "url(#arrow)")
+        .attr("stroke", "grey")
+        .attr("stoke-width", "1.5px");
+
+      this.graph.selectAll("circle").remove();
+      this.graph
+        .selectAll("circle")
+        .data(this.simulation.nodes())
+        .join("circle")
+        .attr("fill", "lightblue")
+        .attr("r", "30")
+        .call(
+          d3
+            .drag()
+            .on("start", this.nodeDragStart)
+            .on("drag", this.nodeDrag)
+            .on("end", this.nodeDragEnd)
+        );
+
+      this.graph.selectAll("text").remove();
+      this.graph
+        .selectAll("text")
+        .data(this.simulation.nodes())
+        .join("text")
+        .attr("color", "black")
+        .attr("font", "10px san-serif")
+        .attr("x", "0")
+        .attr("y", "0.31em")
+        .attr("text-anchor", "middle")
+        .text(d => d.name);
+
+      this.simulation.alpha(1).restart();
+
+      console.log("simulation restart after updating data");
     },
     setForceProperties() {
-      const element = document.getElementById("test-graph");
-      const width = element.getBoundingClientRect().width;
-      const height = element.getBoundingClientRect().height;
-
       this.simulation
         .force("center")
-        .x(width / 2)
-        .y(height / 2);
+        .x(this.width / 2)
+        .y(this.height / 2);
 
       this.simulation
         .force("charge")
@@ -134,24 +181,43 @@ export default {
       this.simulation
         .force("x")
         .strength(this.forceProperties.forceX.strength)
-        .x(width / 2);
+        .x(this.width / 2);
       this.simulation
         .force("y")
         .strength(this.forceProperties.forceY.strength)
-        .y(height / 2);
+        .y(this.height / 2);
 
       console.log("settings done");
 
       this.simulation.alpha(1).restart();
 
       console.log("simulation restart");
+    },
+    nodeDragStart(node) {
+      if (!d3.event.active) {
+        this.simulation.alphaTarget(0.3).restart();
+      }
+
+      node.fx = node.x;
+      node.fy = node.y;
+    },
+    nodeDrag(node) {
+      node.fx = d3.event.x;
+      node.fy = d3.event.y;
+    },
+    nodeDragEnd(node) {
+      if (!d3.event.active) {
+        this.simulation.alphaTarget(0).restart();
+      }
+
+      node.fx = null;
+      node.fy = null;
     }
   },
   watch: {
     nodes: {
       handler() {
         this.updateSimulation();
-        this.setForceProperties();
       }
     },
     forceProperties: {
